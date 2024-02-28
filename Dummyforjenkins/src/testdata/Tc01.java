bb6bed5dd5fce0e3a5f3d3c2272c10c3ed265738
baskardeepu <baskardeepu@192.168.1.2>
2024-02-28 10:33:30 +0530

    data for jenkins

---
+++ Dummyforjenkins/src/testdata/Tc01.java
@@ -0,0 +1,20 @@
+package testdata;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Tc01 {
+	@Test
+	public void startdrow() throws InterruptedException {
+		System.setProperty("Webdriver.geckodriver.driver", "./driver/geckodriver");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://www.thetestingworld.com/testings/");
+		driver.findElement(By.name("fld_username")).sendKeys("baskar");
+		Thread.sleep(2000);
+		driver.quit();
+	}
+
+}
